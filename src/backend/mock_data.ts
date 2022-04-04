e97ca06959ddb7b1b826a690f565c00a8ef3b581
sergey.lebidko <sergeyler@mail.ru>
2022-04-04 11:27:09 +0300

    Созданы тестовые данные для бэкенда. Правка типов данных

--- src/backend/mock_data.ts
+++ src/backend/mock_data.ts
@@ -1,3 +1,72 @@
-export const MOCK_DATA = {
+import {TDataSet, TMockData} from "./types";
 
+export const MOCK_DATA: TMockData = {
+    [TDataSet.User]: [
+        {
+            id: 1,
+            login: 'thanos',
+            password: 'qwerty',
+            first_name: 'Танос',
+            last_name: 'Завоеватель',
+            avatar: null
+        },
+        {
+            id: 2,
+            login: 'ironman',
+            password: 'qwerty',
+            first_name: 'Тони',
+            last_name: 'Старк',
+            avatar: null
+        }
+    ],
+    [TDataSet.Post]: [
+        {
+            id: 1,
+            user_id: 1,
+            title: 'Камень реальности',
+            text: 'Реальность полна разочарований... Но теперь я сам определяю реальность!',
+            dt_created: 1649059902423
+        },
+        {
+            id: 2,
+            user_id: 2,
+            title: 'Коротко обо мне',
+            text: 'Гений, плэйбой, миллиардер, эрудит, филантроп!...',
+            dt_created: 1649060210268
+        }
+    ],
+    [TDataSet.Comment]: [
+        {
+            id: 1,
+            user_id: 1,
+            post_id: 2,
+            text: 'Такой же как и все мстители! Упрям и обуреваем гордыней!',
+            dt_created: 1649060435634
+        },
+        {
+            id: 2,
+            user_id: 2,
+            post_id: 1,
+            text: 'Да ничего ты не определяешь, фиолетовый!',
+            dt_created: 1649060524966
+        }
+    ],
+    [TDataSet.Tag]: [
+        {
+            id: 1,
+            text: 'Обо мне'
+        }
+    ],
+    [TDataSet.PostTag]: [
+        {
+            id: 1,
+            tag_id: 1,
+            post_id: 1
+        },
+        {
+            id: 2,
+            tag_id: 1,
+            post_id: 2
+        }
+    ]
 }
--- src/backend/types.ts
+++ src/backend/types.ts
@@ -1,3 +1,4 @@
+// Методы для доступа к базе данных
 export enum TMethods {
     Get = 'Get',
     Post = 'Post',
@@ -5,11 +6,13 @@ export enum TMethods {
     Delete = 'Delete'
 }
 
-export enum TUrls {
+// Основные наборы данных
+export enum TDataSet {
     User = 'User',
     Post = 'Post',
     Comment = 'Comment',
-    Tag = 'Tag'
+    Tag = 'Tag',
+    PostTag = 'PostTag'
 }
 
 export type TUser = {
@@ -17,12 +20,14 @@ export type TUser = {
     login: string,
     password: string,
     first_name: string,
-    last_name: string
+    last_name: string,
+    avatar: null | string
 }
 
 export type TPost = {
     id: number,
     user_id: number,
+    title: string
     text: string,
     dt_created: number
 }
@@ -41,6 +46,16 @@ export type TTag = {
 }
 
 export type TPostTag = {
+    id: number,
     tag_id: number,
     post_id: number
+}
+
+// Тип данных для представления стартового пресета данных
+export type TMockData = {
+    [TDataSet.User]: TUser[],
+    [TDataSet.Post]: TPost[],
+    [TDataSet.Comment]: TComment[],
+    [TDataSet.Tag]: TTag[],
+    [TDataSet.PostTag]: TPostTag[]
 }
