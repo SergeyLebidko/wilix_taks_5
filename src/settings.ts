710e4545a17b0b24d4a2b631723bfb83ee596fb1
sergey.lebidko <sergeyler@mail.ru>
2022-04-06 08:46:11 +0300

    Create code to load lists of data

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 // Время "сетевой задержки" и миллисекундах для эмулатора бэкенда
-export const BACKEND_TIMEOUT = 3000;
+export const BACKEND_TIMEOUT = 500;
 
 // Имя ключа в local storage для хранения базы данных
 export const DB_NAME = 'backend'
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,8 @@
 import {configureStore, createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import backend from './backend/backend';
-import {TUrls, TUser} from './backend/types';
+import {TComment, TPost, TPostTag, TTag, TUrls, TUser} from './backend/types';
 
+// Список пользователей
 export const loadUserList = createAsyncThunk(
     'user_list',
     async () => {
@@ -12,7 +13,7 @@ export const loadUserList = createAsyncThunk(
 const userListSlice = createSlice({
     name: 'user_list',
     initialState: {
-        status: 'done',
+        status: 'pending',
         data: [] as TUser[]
     },
     reducers: {},
@@ -28,9 +29,118 @@ const userListSlice = createSlice({
     }
 });
 
+// Список постов
+export const loadPostList = createAsyncThunk(
+    'post_list',
+    async () => {
+        return backend.fetch(TUrls.GetPostList);
+    }
+);
+
+const postListSlice = createSlice({
+    name: 'post_list',
+    initialState: {
+        status: 'pending',
+        data: [] as TPost[]
+    },
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(loadPostList.pending, state => {
+                state.status = 'pending'
+            }).addCase(loadPostList.fulfilled, (state, action) => {
+            state.status = 'done';
+            state.data = action.payload as TPost[];
+        });
+    }
+});
+
+// Список комментариев
+export const loadCommentList = createAsyncThunk(
+    'comment_list',
+    async () => {
+        return backend.fetch(TUrls.GetCommentList);
+    }
+);
+
+const commentListSlice = createSlice({
+    name: 'comment_list',
+    initialState: {
+        status: 'pending',
+        data: [] as TComment[]
+    },
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(loadCommentList.pending, state => {
+                state.status = 'pending'
+            })
+            .addCase(loadCommentList.fulfilled, (state, action) => {
+                state.status = 'done';
+                state.data = action.payload as TComment[];
+            });
+    }
+});
+
+// Список тегов
+export const loadTagList = createAsyncThunk(
+    'tag_list',
+    async () => {
+        return backend.fetch(TUrls.GetTagList)
+    }
+);
+
+const tagListSlice = createSlice({
+    name: 'tag_list',
+    initialState: {
+        status: 'pending',
+        data: [] as TTag[]
+    },
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(loadTagList.pending, state => {
+                state.status = 'pending';
+            })
+            .addCase(loadTagList.fulfilled, (state, action) => {
+                state.status = 'done';
+                state.data = action.payload as TTag[];
+            });
+    }
+});
+
+// Список связей тегов и постов
+export const loadPostTagList = createAsyncThunk(
+    'post_tag_list',
+    async () => {
+        return backend.fetch(TUrls.GetPostTagList);
+    }
+);
+
+const postTagListSlice = createSlice({
+    name: 'post_tag_list',
+    initialState: {
+        status: 'pending',
+        data: [] as TPostTag[]
+    },
+    reducers: {},
+    extraReducers: builder => {
+        builder.addCase(loadPostTagList.pending, state => {
+            state.status = 'pending';
+        }).addCase(loadPostTagList.fulfilled, (state, action) => {
+            state.status = 'done';
+            state.data = action.payload as TPostTag[];
+        });
+    }
+});
+
 const store = configureStore({
     reducer: {
-        'user_list': userListSlice.reducer
+        'user_list': userListSlice.reducer,
+        'post_list': postListSlice.reducer,
+        'comment_list': commentListSlice.reducer,
+        'tag_list': tagListSlice.reducer,
+        'post_tag_list': postTagListSlice.reducer
     }
 });
 
