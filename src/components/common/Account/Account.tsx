c3bd76eedba22696c864f6d8394288004b823abe
sergey.lebidko <sergeyler@mail.ru>
2022-04-07 12:21:49 +0300

    Add user data to Account component

--- src/components/common/Account/Account.tsx
+++ src/components/common/Account/Account.tsx
@@ -1,21 +1,46 @@
 import React from 'react';
-import {Stack} from '@mui/material';
+import {Avatar, Fab, Stack, Typography} from '@mui/material';
+import {Link} from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import {useSelector} from 'react-redux';
 import {loggedUserSelector} from '../../../store';
-import {Link} from '@mui/material';
 
 const Account: React.FC = () => {
     const loggedUser = useSelector(loggedUserSelector);
 
+    const userFullName = `${loggedUser?.first_name} ${loggedUser?.last_name}`;
     return (
-        <Stack direction="row" spacing={1} sx={{color: 'white'}}>
+        <Stack direction="row" spacing={1} sx={{color: 'white', alignItems: 'center'}}>
             {(function () {
                 if (loggedUser) {
                     return (
-                        <div>
-                            Пользователь залогинен!
-                        </div>
+                        <>
+                            <Typography variant="h6" gutterBottom component="div" sx={{m: 0}}>
+                                {userFullName}
+                            </Typography>
+                            {loggedUser.avatar ?
+                                <Avatar
+                                    alt={userFullName}
+                                    src={loggedUser.avatar}
+                                /> :
+                                <Avatar sx={{bgcolor: '#fff', color: '#00BFFF'}}>
+                                    {loggedUser.first_name[0]}{loggedUser.last_name[0]}
+                                </Avatar>
+                            }
+                            <Fab
+                                size="small"
+                                sx={{
+                                    color: '#00BFFF',
+                                    boxShadow: 'none',
+                                    backgroundColor: '#fff',
+                                    '&:active': {
+                                        boxShadow: 'none'
+                                    }
+                                }}>
+                                <LogoutIcon/>
+                            </Fab>
+                        </>
                     );
                 } else {
                     return (
--- src/components/pages/Logout/Logout.tsx
+++ src/components/pages/Logout/Logout.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import {Container, Typography} from '@mui/material';
+import {Stack} from '@mui/material';
 
 const Logout: React.FC = () => {
     return (
-        <Box>
-            Старница ввыхода из учетной записи...
-        </Box>
+        <Container sx={{display: 'flex', justifyContent: 'center'}}>
+            <Stack spacing={2} maxWidth="30em" sx={{width: '25em'}}>
+                <Typography variant="h5" gutterBottom component="div" sx={{textAlign: 'center'}}>
+                    Вы действительно хотите выйти?
+                </Typography>
+            </Stack>
+        </Container>
     );
 };
 
