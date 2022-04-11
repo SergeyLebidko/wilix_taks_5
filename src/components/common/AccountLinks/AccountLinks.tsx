da5b04d2ec87295ed888ae43170d7fbf22963e0f
sergey.lebidko <sergeyler@mail.ru>
2022-04-11 11:52:18 +0300

    Refactor account links styles

--- src/components/common/AccountLinks/AccountLinks.tsx
+++ src/components/common/AccountLinks/AccountLinks.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Link} from '@mui/material';
 
+import './AccountLinks.scss';
+
 const AccountLinks: React.FC = () => {
     return (
         <>
-            <Link sx={{typography: 'body1'}} href="#/login" color="#fff">
+            <Link sx={{typography: 'body1'}} className="account_links" href="#/login">
                 Вход
             </Link>
-            <Link sx={{typography: 'body1'}} href="#/register" color="#fff">
+            <Link sx={{typography: 'body1'}} className="account_links" href="#/register">
                 Регистрация
             </Link>
         </>
