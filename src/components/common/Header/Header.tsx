cdf13cb93efd8423a6fa9e08804b309dc1dbbf35
sergey.lebidko <sergeyler@mail.ru>
2022-04-11 11:57:05 +0300

    Remove logo component

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {Box, Stack} from '@mui/material';
+import {Box, Link, Stack} from '@mui/material';
 
 import {loggedUserSelector} from '../../../redux/selectors';
-import Logo from '../Logo/Logo';
 import AccountLinks from '../AccountLinks/AccountLinks';
 import AccountActions from '../AccountActions/AccountActions';
 import './Header.scss';
@@ -13,7 +12,7 @@ const Header: React.FC = () => {
 
     return (
         <Box className="header">
-            <Logo/>
+            <Link className="header__title" href="#/">Wilix Blog</Link>
             <Stack className="header__content_block" direction="row" spacing={1}>
                 {loggedUser ? <AccountActions/> : <AccountLinks/>}
             </Stack>
