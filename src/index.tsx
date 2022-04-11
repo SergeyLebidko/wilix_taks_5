8e9a37d228c9a71824b1b98e557f982996555b85
sergey.lebidko <sergeyler@mail.ru>
2022-04-11 14:41:31 +0300

    Fix start app in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-
+import {createRoot} from 'react-dom/client';
 import App from './components/App/App';
+import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import store from './redux/store';
 import './backend/backend';
-import {HashRouter} from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
     <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>,
-    document.getElementById('root')
+    </HashRouter>
 );
