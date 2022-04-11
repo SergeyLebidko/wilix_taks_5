ee6bfec468418ba4722d0b5e2c10c897609c210b
sergey.lebidko <sergeyler@mail.ru>
2022-04-11 11:00:37 +0300

    Refactor preloader styles

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import {Box, CircularProgress} from '@mui/material';
 
+import './Preloader.scss';
+
 const Preloader: React.FC = () => {
     return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%'
-        }}>
+        <Box className="preloader">
             <CircularProgress/>
         </Box>
     );
